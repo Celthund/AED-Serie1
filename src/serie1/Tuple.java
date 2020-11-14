8dc4ebb9a739689a101791e5a3598105d1ee94c0
Tiago Silva <tiagosik1994@gmail.com>
2020-11-14 00:16:02 +0000

    Add exercise2 with java classes.

---
+++ src/serie1/Tuple.java
@@ -0,0 +1,10 @@
+package serie1;
+
+class Tuple {
+    public int counter;
+    public String word;
+    Tuple (String word, int counter){
+        this.counter = counter;
+        this.word = word;
+    }
+}
